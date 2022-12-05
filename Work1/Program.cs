5e21d34144bfed02a5377097f635e2047bc0e5ca
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2022-12-05 14:47:48 +0300

    Добавили домашнюю работу по третьему семинару C#

---
+++ Work1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №19. Программа, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Это число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Это число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5)
+{
+    CheckingNumber(number);
+}
+else Console.WriteLine($"Введи правильное число");
